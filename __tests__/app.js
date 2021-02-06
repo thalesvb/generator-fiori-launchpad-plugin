7cafc796a767f54f032391f092446b7e2fea4abf
ThalesVB <thalesvb@live.com>
2021-02-06 10:43:56 +0000

    Minor changes to publish with np.

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,4 +1,3 @@
-/*
 "use strict";
 const path = require("path");
 const assert = require("yeoman-assert");
@@ -8,11 +7,19 @@ describe("generator-fiori-launchpad-plugin:app", () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({ someAnswer: true });
+      .withPrompts({});
   });
 
   it("creates files", () => {
-    Assert.file(['dummyfile.txt']);
+    assert.file(["package.json", "ui5.yaml"]);
+    assert.file(
+      [
+        "manifest.json",
+        "flpSandbox.html",
+        "Component.js",
+        "i18n/i18n.properties",
+        "appconfig/fioriSandboxConfig.json"
+      ].map(file => "webapp/" + file)
+    );
   });
 });
-*/
